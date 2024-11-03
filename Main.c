5ca641502440470ac93792db2a2e6f309e3d1acb
Felipe Matias <74963264+Zibec@users.noreply.github.com>
2024-11-03 14:07:40 -0300

    Imagem de fundo
    Tá parecendo que mudei muita coisa, mas não, ignorem.

--- Main.c
+++ Main.c
@@ -33,124 +33,118 @@ void menu() {
         }           
     }
 }
-   
 
 // Função principal do jogo
-void iniciarJogo() {
-    
-    while (!WindowShouldClose()){
-        Rectangle retangulo = {retangulo_x, retangulo_y,60,100};
+void iniciarJogo(Texture2D backgroundImage) {
+    while (!WindowShouldClose()) {
+        Rectangle retangulo = {retangulo_x, retangulo_y, 60, 100};
+        
         if (IsKeyPressed(KEY_P)) {
-            
             pausa = !pausa;
-       }
-        if(!pausa){
+        }
+        
+        if (!pausa) {
             // Movimento da bola
-            if (IsKeyDown(KEY_UP)) {
+            if (IsKeyDown(KEY_UP)){
                 retangulo_y -= velocidadeBola;
-            }
-            if (IsKeyDown(KEY_DOWN)) {
+                }
+            if (IsKeyDown(KEY_DOWN)){
                 retangulo_y += velocidadeBola;
-            }
-            if (IsKeyDown(KEY_RIGHT)) {
+                }
+            if (IsKeyDown(KEY_RIGHT)){
                 retangulo_x += velocidadeBola;
-            }
-            if (IsKeyDown(KEY_LEFT)) {
+                }
+            if (IsKeyDown(KEY_LEFT)){
                 retangulo_x -= velocidadeBola;
-            }
+                }
             
             // Restringir a bola para não sair da tela
             if (retangulo_x < 0){
                 retangulo_x = 0;
-            }
-                    
+                }
+                
             if (retangulo_y < 0){
                 retangulo_y = 0;
-            }
-                    
-            if (retangulo_x > GetScreenWidth()-60){
-                retangulo_x = GetRenderWidth()-60;
-            } 
-                    
-            if (retangulo_y > GetScreenHeight()-100) {
-                retangulo_y = GetScreenHeight()-100;
-            }
-                    
-            // Voltar ao menu se a tecla P for pressionada
+                }
+                
+            if (retangulo_x > GetScreenWidth() - 60){
+                retangulo_x = GetScreenWidth() - 60;
+                }
+                
+            if (retangulo_y > GetScreenHeight() - 100){
+                retangulo_y = GetScreenHeight() - 100;
+                }
             
             Rectangle retanguloEstatico = {100, 100, 500, 300};
-            //Vector2 vetor = {retangulo_x, retangulo_y};
+             //Vector2 vetor = {retangulo_x, retangulo_y};
             
-            
-            
+            // Colisão com o retângulo estático
             if (CheckCollisionRecs(retangulo, retanguloEstatico)) {
-                // Calcular a penetração em cada direção
-                float penetraçãoEsquerda = (retangulo.x + retangulo.width) - retanguloEstatico.x;
-                float penetraçãoDireita = (retanguloEstatico.x + retanguloEstatico.width) - retangulo.x;
-                float penetraçãoCima = (retangulo.y + retangulo.height) - retanguloEstatico.y;
-                float penetraçãoBaixo = (retanguloEstatico.y + retanguloEstatico.height) - retangulo.y;
+                float penetracaoEsquerda = (retangulo.x + retangulo.width) - retanguloEstatico.x;
+                float penetracaoDireita = (retanguloEstatico.x + retanguloEstatico.width) - retangulo.x;
+                float penetracaoCima = (retangulo.y + retangulo.height) - retanguloEstatico.y;
+                float penetracaoBaixo = (retanguloEstatico.y + retanguloEstatico.height) - retangulo.y;
 
-                // Encontrar a menor penetração para determinar a direção da colisão
-                if (penetraçãoEsquerda < penetraçãoDireita && penetraçãoEsquerda < penetraçãoCima && penetraçãoEsquerda < penetraçãoBaixo) {
+                if (penetracaoEsquerda < penetracaoDireita && penetracaoEsquerda < penetracaoCima && penetracaoEsquerda < penetracaoBaixo) {
                     // Colisão pela esquerda
                     retangulo_x = retanguloEstatico.x - retangulo.width;
-                } 
-                else if (penetraçãoDireita < penetraçãoEsquerda && penetraçãoDireita < penetraçãoCima && penetraçãoDireita < penetraçãoBaixo) {
+                } else if (penetracaoDireita < penetracaoEsquerda && penetracaoDireita < penetracaoCima && penetracaoDireita < penetracaoBaixo) {
                     // Colisão pela direita
                     retangulo_x = retanguloEstatico.x + retanguloEstatico.width;
-                } 
-                else if (penetraçãoCima < penetraçãoBaixo && penetraçãoCima < penetraçãoEsquerda && penetraçãoCima < penetraçãoDireita) {
+                } else if (penetracaoCima < penetracaoBaixo && penetracaoCima < penetracaoEsquerda && penetracaoCima < penetracaoDireita) {
                     // Colisão por cima
                     retangulo_y = retanguloEstatico.y - retangulo.height;
-                } 
-                else if (penetraçãoBaixo < penetraçãoCima && penetraçãoBaixo < penetraçãoEsquerda && penetraçãoBaixo < penetraçãoDireita) {
+                } else if (penetracaoBaixo < penetracaoCima && penetracaoBaixo < penetracaoEsquerda && penetracaoBaixo < penetracaoDireita) {
                     // Colisão por baixo
                     retangulo_y = retanguloEstatico.y + retanguloEstatico.height;
                 }
-}
-                
-            
+            }
             
             // Desenho do jogo
             BeginDrawing();
             ClearBackground(RAYWHITE);
             
+            // Desenha a imagem de fundo
+            DrawTexture(backgroundImage, 0, 0, WHITE);
+            
             DrawRectangleRec(retanguloEstatico, BLACK);
-        
             DrawRectangleRec(retangulo, BLUE);
             
             EndDrawing();
         }
-      
-       if(pausa){
-           BeginDrawing();
+        
+        if (pausa) {
+            BeginDrawing();
             
-           DrawText("PAUSE", 500, 290, 70, GOLD);
-                
-           EndDrawing();
+            DrawText("PAUSE", 500, 290, 70, GOLD);
+            
+            EndDrawing();
         }
     }
 }
+
 int main(void) {
     // Configuração inicial da janela e posição inicial da bola
     int larguraTela = 1280, alturaTela = 720;
     retangulo_x = larguraTela / 2;
     retangulo_y = alturaTela / 2;
-    
+        
     InitWindow(larguraTela, alturaTela, "Uma Noite no Castelo");
     SetTargetFPS(60);
-
+    
     // Carregar recursos (ex.: imagens, texturas) se necessário
     
     // Texture2D backgroundImage = LoadTexture("caminho/para/imagem.png");
+    // Carregar a imagem de fundo
+    Texture2D backgroundImage = LoadTexture("background.png");
 
     // Exibir menu e iniciar o jogo
     menu();
-    iniciarJogo();
+    iniciarJogo(backgroundImage);
     
     // Apartir daqui tem que descarregar recursos e fechar a janela
-    
-    // UnloadTexture(backgroundImage);
+    // Descarregar a imagem de fundo e fechar a janela
+    UnloadTexture(backgroundImage);
     CloseWindow();
     
     return 0;
